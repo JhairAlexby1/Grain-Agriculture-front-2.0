4f9fcd7dcd947e2d312782054c75092ede1d244e
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-11-18 21:46:38 -0600

    prediccion de movimiento agregada

--- src/pages/Prediction.jsx
+++ src/pages/Prediction.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Menu from '../components/Menu';
 import PredictionContent from '../components/PredictionComponents/PredictionContent';
 
 export const Prediction = () => {
+  const [probability, setProbability] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/statistics/movement-prediction', { withCredentials: true })
+      .then(response => {
+        setProbability(response.data.probability);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
   return (
     <div className="flex h-screen bg-gray-100">
       <Menu />
-      <PredictionContent />
+      <div className="flex flex-col items-center justify-center w-full">
+        <PredictionContent />
+        {probability !== null && (
+          <div className="mt-4 p-4 bg-white shadow-md rounded">
+            <p className="text-xl font-semibold">Probabilidad de Movimiento en el dia: {probability}%</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
